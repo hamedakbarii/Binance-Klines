11271fa516eca9771bf3cc0fadae57846d8e1d84
hamedakbari <hamedakbariwork@gmail.com>
2024-10-26 19:23:38 +0330

    not found page updated

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex justify-center items-center bg-white dark:bg-black">
       <div className="flex flex-col justify-center items-center gap-4">
         <img src="./images/logo/logo.png" alt="logo" className="rounded-full" />
-        <h1 className="text-xl font-semibold">Sorry!</h1>
-        <p>404! This page is not available</p>
-        <Link to={"/"} className="text-sm underline">
+        <h1 className="text-xl font-semibold text-black dark:text-white">
+          Sorry!
+        </h1>
+        <p className="text-black dark:text-white">
+          404! This page is not available
+        </p>
+        <Link to={"/"} className="text-sm underline text-black dark:text-white">
           Let's get back to Home
         </Link>
       </div>
